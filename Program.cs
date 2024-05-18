abd4e38e955569ab8d0e757c08e0a5c8ddb4365f
Victor <we5705988@gmail.com>
2024-05-18 10:24:44 -0300

    Contador de vogais

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Insira uma string aleatória: ");
+        string palavra = Console.ReadLine()!;
+
+        int contador = 0;
+
+        foreach(char str in palavra) {
+
+            if ("aeiouAEIOU".Contains(str)) {
+                contador++; // Aumenta o contador;
+            }
+        }
+
+        Console.WriteLine("Total de vogais encontradas na sua palavra: " + Convert.ToString(contador)); // Exibi o tanto de vogais encontrado.
+    }
+}
+
